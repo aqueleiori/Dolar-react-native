16b7da275f6abe49e16aec0bf5c1e995ed693973
aqueleiori <aqueleyori@gmail.com>
2022-03-23 03:04:48 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
+import React, { useState, useEffect } from "react"
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState("")
+  const [bid, setBid] = useState("")
+
+  useEffect(() => {
+      fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+      .then(response => response.json().then(res => {
+            setName(res.USDBRL.name)
+            setBid(parseFloat(res.USDBRL.bid).toFixed(2))
+      }))
+      })
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>USD TO BRL</Text>
+      <Text style={styles.subTitle}>{bid}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +29,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontWeight:'bold',
+    fontSize: 50,
+  },
+  subTitle: {
+    fontWeight:'bold',
+    fontSize: 35,
+
+  }
 });
